a7d878d3a4d46af4e60c32ec8d47a06c49bff144
Fethi bey Abi ayad <abi_ayad.fethi_bey@courrier.uqam.ca>
2021-07-22 23:05:03 -0400

    mapping de la table testablishments dans la classe Establi

--- src/main/java/com/uqam/model/Establishment.java
+++ src/main/java/com/uqam/model/Establishment.java
@@ -1,15 +1,30 @@
 package com.uqam.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tEstablishments")
 public class Establishment {
 
-    private String id;
+    @Id @GeneratedValue( strategy= GenerationType.IDENTITY )
+    private long id;
+
+    private String identification;
     private String designation ;
 
     public String getId() {
-        return id;
+        return identification;
     }
 
     public String getDesignation() {
         return designation;
+    }
+
+    @Override
+    public String toString() {
+        return "Establishment{" +
+                "identification='" + identification + '\'' +
+                ", designation='" + designation + '\'' +
+                '}';
     }
 }
