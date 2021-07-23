c92ee837575892bb9b47215ae99e7f24cdc08f1d
Fethi bey Abi ayad <abi_ayad.fethi_bey@courrier.uqam.ca>
2021-07-22 23:18:43 -0400

    mapping de la classe Patient

--- src/main/java/com/uqam/model/Patient.java
+++ src/main/java/com/uqam/model/Patient.java
@@ -1,17 +1,30 @@
 package com.uqam.model;
 
-import java.util.Date;
+import javax.persistence.*;
+import java.sql.Date;
 import java.util.List;
 
+@Entity
+@Table(name = "tPatients")
 public class Patient {
 
+    @Id
+    @GeneratedValue( strategy= GenerationType.IDENTITY )
+    private long id;
     private String firstname;
     private String lastname;
     private Gender gender;
     private Date dateOfBirth;
     private String birthCity;
     private String insuranceNumber;
+
+    @OneToOne (cascade = CascadeType.ALL)
+    @JoinColumn( name="contact")
     private Contact contact;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable( name = "tParents", joinColumns = @JoinColumn( name = "fils" ),
+            inverseJoinColumns = @JoinColumn( name = "parent" ) )
     private List<Patient> parents;
 
     public String getFirstname() {
@@ -40,5 +53,19 @@ public class Patient {
 
     public Contact getContact() {
         return contact;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", gender=" + gender +
+                ", dateOfBirth=" + dateOfBirth +
+                ", birthCity='" + birthCity + '\'' +
+                ", insuranceNumber='" + insuranceNumber + '\'' +
+                ", contact=" + contact +
+                ", parents=" + parents +
+                '}';
     }
 }
