e5aa8e78566c3fe24b2279d519193031b615bf58
Fethi bey Abi ayad <abi_ayad.fethi_bey@courrier.uqam.ca>
2021-07-22 23:18:34 -0400

    mapping de la classe Folder

--- src/main/java/com/uqam/model/Folder.java
+++ src/main/java/com/uqam/model/Folder.java
@@ -1,11 +1,26 @@
 package com.uqam.model;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "tFolders")
 public class Folder {
 
+    @Id
+    @GeneratedValue( strategy= GenerationType.IDENTITY )
+    private long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn( name="owner")
     private Patient owner;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="folder", referencedColumnName="id")
     private List<Visit> visits;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="folder", referencedColumnName="id")
     private List<Antecedent> antecedents;
 
     public Patient getOwner() {
@@ -18,5 +33,14 @@ public class Folder {
 
     public List<Antecedent> getAntecedents() {
         return antecedents;
+    }
+
+    @Override
+    public String toString() {
+        return "Folder{" +
+                "owner=" + owner +
+                ", visits=" + visits +
+                ", antecedents=" + antecedents +
+                '}';
     }
 }
