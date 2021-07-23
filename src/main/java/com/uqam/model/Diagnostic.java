80fd5340a79d8e0b8a324ec703726e260bcc0943
Fethi bey Abi ayad <abi_ayad.fethi_bey@courrier.uqam.ca>
2021-07-22 23:18:25 -0400

    mapping de la classe Diagnostic

--- src/main/java/com/uqam/model/Diagnostic.java
+++ src/main/java/com/uqam/model/Diagnostic.java
@@ -1,11 +1,24 @@
 package com.uqam.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tPatients")
 public class Diagnostic {
 
+    @Id
+    @GeneratedValue( strategy= GenerationType.IDENTITY )
+    private long id;
     private String designation;
-
 
     public String getDesignation() {
         return designation;
+    }
+
+    @Override
+    public String toString() {
+        return "Diagnostic{" +
+                "designation='" + designation + '\'' +
+                '}';
     }
 }
