c3aba11538f2f81c3ef23d6b6c719eae4a750d9a
ck191923 <ck191923>
2021-08-10 21:23:38 -0400

    tests pour la classe Contact

--- src/test/java/com/uqam/model/ContactTest.java
+++ src/test/java/com/uqam/model/ContactTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class ContactTest {
 
-    Contact contact = new Contact("5825henribourassa","5146492231","testemail@gmail.com");
+    Contact contact = new Contact("5825henribourassa","5144922317","testemail@gmail.com");
 
     @Test
     void testValidAddress(){
@@ -14,6 +14,13 @@ public class ContactTest {
 
     @Test
     void testValidPhone(){
-
+        Assertions.assertTrue(contact.validPhone(contact.getPhone()));
     }
+    @Test
+    void testValidEmail(){
+        Assertions.assertTrue(contact.validEmail(contact.getEmail()));
+    }
+
+    Contact contact2 = contact.clone();
 }
+
