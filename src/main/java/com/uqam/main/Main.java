d573b644fe7e3e32b73ac6d77da0d4acbbe51028
ck191923 <ck191923@ens.uqam.ca>
2021-08-13 19:35:43 -0400

    correction main

--- src/main/java/com/uqam/main/Main.java
+++ src/main/java/com/uqam/main/Main.java
@@ -10,10 +10,8 @@ import java.util.List;
 public class Main  {
 
     public static void main(String[] args) {
-      //MyApp.main(args);
-        Treatment treatment = new Treatment("Vaccin Moderna");
+      MyApp.main(args);
 
-        System.out.println(treatment);
 
     }
 }
